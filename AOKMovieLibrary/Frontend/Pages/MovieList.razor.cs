a2a899189f7cb55994aff17d1473dccf5f96e722
Simon Mai <s.mai@affinis.de>
2024-11-21 15:12:07 +0100

    add local and session storage

--- AOKMovieLibrary/Frontend/Pages/MovieList.razor.cs
+++ AOKMovieLibrary/Frontend/Pages/MovieList.razor.cs
@@ -1,10 +1,16 @@
-﻿namespace AOKMovieLibrary.Frontend.Pages;
+﻿using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
+
+namespace AOKMovieLibrary.Frontend.Pages;
 
 public partial class MovieList
 {
     [Inject] NavigationManager NavigationManager { get; set; } = null!;
 
     [Inject] MovieStateService MovieStateService { get; set; } = null!;
+
+    [Inject] ProtectedLocalStorage ProtectedLocalStorage { get; set; } = null!;
+
+    [Inject] ProtectedSessionStorage ProtectedSessionStorage { get; set; } = null!;
 
     [Inject] private IMovieService _movieService { get; set; } = null!;
 
@@ -13,33 +19,43 @@ public partial class MovieList
     protected override async Task OnInitializedAsync()
     {
         Movies = (await _movieService.GetMoviesAsync()).Select(movie => movie.MapToMovieOverview()).ToList();
+
+        // use local storage if prefered
+        var genre = await ProtectedSessionStorage.GetAsync<MovieGenre>(nameof(MovieGenre));
+
+        if (!genre.Success || genre.Value == MovieGenre.None)
+        {
+            SelectedGenreFilter = MovieGenre.None;
+        }
+
+        SelectedGenreFilter = genre.Value;
     }
 
     // Component specific filtering
-    private MovieGenre SelectedGenreFilter { get; set; }
+    //private MovieGenre SelectedGenreFilter { get; set; }
 
-    private List<MovieOverviewData> FilteredMovies()
-    {
-        if (SelectedGenreFilter == MovieGenre.None)
-        {
-            return Movies;
-        }
+    //private List<MovieOverviewData> FilteredMovies()
+    //{
+    //    if (SelectedGenreFilter == MovieGenre.None)
+    //    {
+    //        return Movies;
+    //    }
 
-        return Movies
-        .Where(m => m.Genre.HasFlag(SelectedGenreFilter))
-        .ToList();
-    }
+    //    return Movies
+    //    .Where(m => m.Genre.HasFlag(SelectedGenreFilter))
+    //    .ToList();
+    //}
 
-    private void OnGenreFilterChanged(ChangeEventArgs e)
-    {
-        if (string.IsNullOrEmpty(e.Value?.ToString()))
-        {
-            SelectedGenreFilter = MovieGenre.None;
-            return;
-        }
+    //private void OnGenreFilterChanged(ChangeEventArgs e)
+    //{
+    //    if (string.IsNullOrEmpty(e.Value?.ToString()))
+    //    {
+    //        SelectedGenreFilter = MovieGenre.None;
+    //        return;
+    //    }
 
-        SelectedGenreFilter = Enum.Parse<MovieGenre>(e.Value.ToString());
-    }
+    //    SelectedGenreFilter = Enum.Parse<MovieGenre>(e.Value.ToString());
+    //}
 
     // State service filtering
 
@@ -64,6 +80,38 @@ public partial class MovieList
 
     //    MovieStateService.CurrentGenreFilter = Enum.Parse<MovieGenre>(e.Value.ToString());
     //}
+
+    // Storage/Session specific filtering
+    private MovieGenre SelectedGenreFilter { get; set; }
+
+    private List<MovieOverviewData> FilteredMovies()
+    {
+        if (SelectedGenreFilter == MovieGenre.None)
+        {
+            return Movies;
+        }
+
+        return Movies
+        .Where(m => m.Genre.HasFlag(SelectedGenreFilter))
+        .ToList();
+    }
+
+    private async Task OnGenreFilterChanged(ChangeEventArgs e)
+    {
+        if (string.IsNullOrEmpty(e.Value?.ToString()))
+        {
+            // use local storage if prefered
+            await ProtectedSessionStorage.SetAsync(nameof(MovieGenre), MovieGenre.None);
+            SelectedGenreFilter = MovieGenre.None;
+            return;
+        }
+
+        MovieGenre genre = Enum.Parse<MovieGenre>(e.Value.ToString());
+
+        // use local storage if prefered
+        await ProtectedSessionStorage.SetAsync(nameof(MovieGenre), genre);
+        SelectedGenreFilter = genre;
+    }
 
     private void NavigateToDetails(MovieOverviewData movie)
     {
